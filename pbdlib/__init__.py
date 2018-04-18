ef1886fce9555e7018c9e38b1ff04053bc139ff0
pignate <emmanuel.pignat@idiap.ch>
2018-04-18 18:55:54 +0200

    make gui optional if no tkinter

--- pbdlib/__init__.py
+++ pbdlib/__init__.py
@@ -11,7 +11,15 @@ from .pylqr import *
 from .poglqr import PoGLQR
 from .mtmm import MTMM, VBayesianGMM
 
-import gui
+try:
+	import gui
+except ImportError as e:
+	print "Could not import gui: {0}".format(e.message)
+	print "run : sudo apt-get install tkinter"
+except:
+	print "Unexpected error:", sys.exc_info()[0]
+	raise
+
 import utils
 import plot
 
