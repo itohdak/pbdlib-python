dc770fe6e2062cdee480fa1a3c39947849524ab6
pignate <emmanuel.pignat@idiap.ch>
2018-05-02 15:36:13 +0200

    various
    (cherry picked from commit b1f652a)

--- pbdlib/__init__.py
+++ pbdlib/__init__.py
@@ -10,8 +10,18 @@ from .plot import *
 from .pylqr import *
 from .poglqr import PoGLQR
 from .mtmm import MTMM, VBayesianGMM
+from .lqr import LQR
+from .multi_pogmm import MultiPoGHSMM, MultiPoGMM
 
-import gui
+try:
+	import gui
+except ImportError as e:
+	print "Could not import gui: {0}".format(e.message)
+	print "run : sudo apt-get install tkinter"
+except:
+	print "Unexpected error:", sys.exc_info()[0]
+	raise
+
 import utils
 import plot
 
